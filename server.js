dd7842364088071d15233b57e68dab03e4876d00
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-05-02 01:41:01 -0400

    Initial files set up, and app is up and running

---
+++ server.js
@@ -0,0 +1,39 @@
+// DEPENDENCIES //
+const express = require('express');
+const methodOverride  = require('method-override');
+const mongoose = require ('mongoose');
+require('dotenv').config();
+const app = express ();
+const db = mongoose.connection;
+const PORT = process.env.PORT || 3000;
+
+// DATABASE //
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/intouchapp';
+
+// CONNECT TO MONGO //
+mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
+
+// ERROR / SUCCESS //
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// open the connection to mongo
+db.on('open' , ()=>{});
+
+// MIDDLEWARE //
+//use public folder for static assets
+app.use(express.static('public'));
+// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+//use method override
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+
+// ROUTES //
+app.get('/' , (req, res) => {
+  res.send('in.Touch app is up and running');
+});
+
+// LISTENER //
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
